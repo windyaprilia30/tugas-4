e8063d0bd96e74ab5d1abcd80eeea59af6a661f5
windyaprilia30 <whindyavrilia665@gmail.com>
2024-10-21 16:38:31 +0800

    Add files via upload

---
+++ #matriks.py
@@ -0,0 +1,22 @@
+#matriks
+A= [[7,5]]
+B= [[3],[8]]
+#cetak detail matriks
+print (A)
+print ("baris matriks A adalah ", A)
+print ("kolom matriks A adalah ", A)
+
+print (B)
+print ("baris matriks B adalah ", B)
+print ("kolom matriks B adalah ", B)
+
+#cek kondisi matriks untuk perkalian 
+if (A == B):
+    print ("matriks A dengan B tidak dapat dikalikan")
+else:
+    print ("matriks A dengan B dapat dikalikan")
+    #print (A[0][0])
+    #print (A[0][0])
+    #print (A[0][1])
+    #print (A[1][0])
+    C = A
