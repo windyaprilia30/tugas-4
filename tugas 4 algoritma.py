6ad261012545e70ff4264da8e825f46fad77aba7
windyaprilia30 <whindyavrilia665@gmail.com>
2024-10-21 16:43:47 +0800

    tugas 4 algoritma.py

--- tugas 4 algoritma.py
+++ tugas 4 algoritma.py
@@ -1,43 +1,47 @@
-def input_matrix(rows, cols):
-    matrix = []
-    for i in range(rows): 
-        row = list(map(int, input(f"input row {i+1} (space-separated): ").split()))
-        matrix.append(row)
-    return matrix
+# tentukan ordo matriks A
+baris_A=int(input("masukkan ukuran baris matriks A= "))
+kolom_A=int(input("masukkan ukuran kolom matriks A= "))
 
-def print_matrix(matrix):
-    for row in matrix:
-        print(" ".join(map(str, row)))
+#Tentukan ordo matriks B
+baris_B=int(input("masukkan ukuran baris matriks B= "))
+kolom_B=int(input("masukkan ukuran kolom matriks B= "))
 
-def multiply_matrices(A, B):
-    mA = len(A)
-    nA = len(A[0])
-    mB = len(B)
-    nB = len(B[0])
-    
-    C = [[0] * nB for _ in range(mA)]
-
-    for i in range(mA):
-        for j in range(nB):
-            for k in range(nA):
-                C[i][j] += A[i][k] * B[k][j]
-
-    return C
-
-
-mA = int(input("masukkan jumlah baris untuk matriks A"))
-nA = int(input("masukkan jumlah kolom untuk matriks A"))
-A = input_matrix(mA, nA)
-
-mB = int(input("masukkan jumlah baris untuk matriks B"))
-nB = int(input("masukkan jumlah kolom untuk matriks B"))
-B = input_matrix(mB, nB)
-
-if nA == mB:
-    print("matriks tidak dapat dikalikan")
+if (kolom_A!=baris_B):
+    print ("matriks tidak dapat dikalikan!")
 else:
-    C = multiply_matrices(A, B)
-    print("hasil perkalian matriks C")
-    print_matrix(C)
+    print ("masukkan elemen matriks A : ")
+    A = []
+    for i in range (baris_A):
+        baris=[]
+        for j in range (kolom_A):
+            value = int(input("elemen matriks A: "))
+            baris.append(value)
+            A.append(baris)
+            
+print("masukkan elemen matriks B ")
+B = []
+for i in range (baris_B):
+    baris=[]
+    for j in range (kolom_B):
+        value = int(input("elemen matriks B: "))
+        baris.append(value)
+        B.append(baris)
+
+    print ("matriks A : ", A)
+    print ("matriks B : ", B)
+
+
+hasil_kali = [[0 for _ in range(kolom_B)] for _ in range(baris_A)]
+for i in range(baris_A):
+    for j in range(kolom_B):
+        for k in range(kolom_A):
+            hasil_kali[i][j]+=A[i][k]*B[k][j]
+
+print ("hasil perkalian matriks A dengan B adalah ", hasil_kali)
+
+
+
+    
+  
 
 
